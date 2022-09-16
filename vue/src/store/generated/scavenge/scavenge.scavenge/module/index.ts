d9c3d5fe77dea927ff288d1e27efeaf064881305
olsenmatthew <olsenmatthew780@gmail.com>
2022-09-16 16:31:59 -0400

    add scavenge types

--- vue/src/store/generated/scavenge/scavenge.scavenge/module/index.ts
+++ vue/src/store/generated/scavenge/scavenge.scavenge/module/index.ts
@@ -4,15 +4,15 @@ import { StdFee } from "@cosmjs/launchpad";
 import { SigningStargateClient } from "@cosmjs/stargate";
 import { Registry, OfflineSigner, EncodeObject, DirectSecp256k1HdWallet } from "@cosmjs/proto-signing";
 import { Api } from "./rest";
-import { MsgCommitSolution } from "./types/scavenge/tx";
 import { MsgSubmitScavenge } from "./types/scavenge/tx";
 import { MsgRevealSolution } from "./types/scavenge/tx";
+import { MsgCommitSolution } from "./types/scavenge/tx";
 
 
 const types = [
-  ["/scavenge.scavenge.MsgCommitSolution", MsgCommitSolution],
   ["/scavenge.scavenge.MsgSubmitScavenge", MsgSubmitScavenge],
   ["/scavenge.scavenge.MsgRevealSolution", MsgRevealSolution],
+  ["/scavenge.scavenge.MsgCommitSolution", MsgCommitSolution],
   
 ];
 export const MissingWalletError = new Error("wallet is required");
@@ -45,9 +45,9 @@ const txClient = async (wallet: OfflineSigner, { addr: addr }: TxClientOptions =
 
   return {
     signAndBroadcast: (msgs: EncodeObject[], { fee, memo }: SignAndBroadcastOptions = {fee: defaultFee, memo: ""}) => client.signAndBroadcast(address, msgs, fee,memo),
-    msgCommitSolution: (data: MsgCommitSolution): EncodeObject => ({ typeUrl: "/scavenge.scavenge.MsgCommitSolution", value: MsgCommitSolution.fromPartial( data ) }),
     msgSubmitScavenge: (data: MsgSubmitScavenge): EncodeObject => ({ typeUrl: "/scavenge.scavenge.MsgSubmitScavenge", value: MsgSubmitScavenge.fromPartial( data ) }),
     msgRevealSolution: (data: MsgRevealSolution): EncodeObject => ({ typeUrl: "/scavenge.scavenge.MsgRevealSolution", value: MsgRevealSolution.fromPartial( data ) }),
+    msgCommitSolution: (data: MsgCommitSolution): EncodeObject => ({ typeUrl: "/scavenge.scavenge.MsgCommitSolution", value: MsgCommitSolution.fromPartial( data ) }),
     
   };
 };
